a98817a8d0d1827362ec427074b30edd3e2fc0e1
Tobias Koch <t.koch@tk-software.de>
2023-12-27 12:28:42 +0100

    Adding version package (#1)

---
+++ version/version.go
@@ -0,0 +1,29 @@
+// Package version provides the application version
+package version
+
+import "fmt"
+
+const (
+	Major    int = 1 // The major version number
+	Minor    int = 0 // The minor version number
+	Revision int = 0 // The revision number
+)
+
+var (
+	Build string = "" // The build number
+)
+
+// Short returns the version number in the format Major.Minor.Revision.
+func Short() string {
+	return fmt.Sprintf("%d.%d.%d", Major, Minor, Revision)
+}
+
+// Version returns the version number in the format Major.Minor.Revision.Build.
+func Version() string {
+	return fmt.Sprintf("%d.%d.%d.%s", Major, Minor, Revision, Build)
+}
+
+// Long returns the version number in the format Major.Minor.Revision (build Build).
+func Long() string {
+	return fmt.Sprintf("%d.%d.%d (build %s)", Major, Minor, Revision, Build)
+}

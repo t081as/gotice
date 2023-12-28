c37eda7c80821dee13bae91013a36773e9bdcb43
Tobias Koch <t.koch@tk-software.de>
2023-12-28 23:20:02 +0100

    Adding mage lint command (#1)

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module pkg.tk-software.de/gotice
 
 go 1.21.5
 
-require pkg.tk-software.de/spartan v1.1.0 // indirect
+require (
+	github.com/magefile/mage v1.15.0
+	pkg.tk-software.de/spartan v1.1.0 // indirect
+)
---
+++ magefiles/lint.go
@@ -0,0 +1,13 @@
+//go:build mage
+
+package main
+
+import "github.com/magefile/mage/sh"
+
+// The version of golangci/golangci-lint that shall be used
+const LinterVersion string = "v1.55.2"
+
+// Lint checks the source code by executing golangci-lint.
+func Lint() error {
+	return sh.RunV("go", "run", "github.com/golangci/golangci-lint/cmd/golangci-lint@"+LinterVersion, "run")
+}

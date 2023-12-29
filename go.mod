957b87913a316beed9e2d05c5619749c8da20a72
Tobias Koch <t.koch@tk-software.de>
2023-12-29 11:11:19 +0100

    Switching to spartan v1.2.0 (#1)

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module pkg.tk-software.de/gotice
 
 go 1.21.5
 
-require (
-	github.com/magefile/mage v1.15.0
-	pkg.tk-software.de/spartan v1.1.0 // indirect
-)
+require github.com/magefile/mage v1.15.0
+
+require pkg.tk-software.de/spartan v1.2.0 // indirect
---
+++ magefiles/magefile.go
@@ -0,0 +1,13 @@
+//go:build mage
+
+package main
+
+import "pkg.tk-software.de/spartan/build"
+
+// The application name.
+const AppName string = "gotice"
+
+// The build targets.
+var Targets []build.Target = []build.Target{
+	{Os: "windows", Arch: "amd64", Name: AppName + ".exe"},
+}

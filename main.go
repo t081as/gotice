5304850fa2df41e837ebd9c03b5ac83d4fbdd50c
Tobias Koch <t.koch@tk-software.de>
2024-01-03 10:28:22 +0100

    Adding gomod module (#1)

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
+
+	"pkg.tk-software.de/gomod"
+	"pkg.tk-software.de/spartan/io/file"
 )
 
 var help *bool = flag.Bool("help", false, "Displays the command line help")
@@ -29,9 +33,22 @@ func main() {
 
 	src := flag.Arg(0)
 	dst := flag.Arg(1)
+	modf := filepath.Join(src, "go.mod")
+
+	if !file.Exists(modf) {
+		fmt.Fprintf(os.Stderr, "ERROR: file %s not found\n", modf)
+		return
+	}
+
+	mod, err := gomod.NewFromDir(src)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: unable to parse %s: %s\n", modf, err)
+		return
+	}
 
 	fmt.Println("Project directory:", src)
 	fmt.Println("Destination file:", dst)
+	fmt.Println("Project:", mod.Module.Path)
 }
 
 func usage() {

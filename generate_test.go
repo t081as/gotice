2319aacd585c263ad42f3ac32456284bc08ea4b3
Tobias Koch <t.koch@tk-software.de>
2024-01-06 10:37:45 +0100

    Improving test (#9)

--- generate_test.go
+++ generate_test.go
@@ -4,17 +4,35 @@
 
 package main
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"pkg.tk-software.de/spartan/io/file"
+)
 
 func TestGenerateCommand(t *testing.T) {
+	tmpdir, err := os.MkdirTemp("", "gotice")
+	if err != nil {
+		t.Fatalf("Expected no error while creating temp dir, got %s", err)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	tmpfile := filepath.Join(tmpdir, "NOTICE.txt")
+
 	args := []string{
 		"param0",
 		"generate",
 		"./", // detects the gotice project
-		"./NOTICE.txt",
+		tmpfile,
 	}
 
 	if err := exec(args); err != nil {
 		t.Errorf("Expected no error, got %s", err)
+	}
+
+	if !file.Exists(tmpfile) {
+		t.Errorf("Expected file %s, got none", tmpfile)
 	}
 }

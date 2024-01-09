aeb6bcb358aa515ff0169fc3d838a02c9d4111cd
Tobias Koch <t.koch@tk-software.de>
2024-01-09 17:06:31 +0100

    Adding read and write methods (#10)

--- notice/options.go
+++ notice/options.go
@@ -4,6 +4,11 @@
 
 package notice
 
+import (
+	"encoding/json"
+	"io"
+)
+
 // Options represents options for generating a notice file.
 type Options struct {
 	Template string `json:"template"` // the template that shall be used
@@ -14,4 +19,30 @@ func NewOptions() *Options {
 	return &Options{
 		Template: "built-in:txt",
 	}
+}
+
+// WriteOptions writes the given options to the given writer w using the json format.
+func WriteOptions(w io.Writer, o *Options) error {
+	b, err := json.Marshal(o)
+	if err != nil {
+		return err
+	}
+
+	if _, err := w.Write(b); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ReadOptions reads options from the reader r in the json format.
+func ReadOptions(r io.Reader) (*Options, error) {
+	o := NewOptions()
+	dec := json.NewDecoder(r)
+
+	if err := dec.Decode(&o); err != nil {
+		return nil, err
+	}
+
+	return o, nil
 }

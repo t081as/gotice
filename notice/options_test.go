c61c2455f30e618dc19c6d8a388f97edf444ed91
Tobias Koch <t.koch@tk-software.de>
2024-01-10 14:33:56 +0100

    Adding options test (#10)

---
+++ notice/options_test.go
@@ -0,0 +1,35 @@
+// Copyright 2023-2024 Tobias Koch. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package notice
+
+import (
+	"io"
+	"os"
+	"testing"
+)
+
+func TestReadWrite(t *testing.T) {
+	f, err := os.CreateTemp("", "")
+	if err != nil {
+		t.Fatalf("Creation of temp file failed: %s", err)
+	}
+	defer os.Remove(f.Name())
+
+	o := NewOptions()
+	if err := WriteOptions(f, o); err != nil {
+		t.Fatalf("Writing options failed: %s", err)
+	}
+
+	f.Seek(io.SeekStart, 0)
+
+	no, err := ReadOptions(f)
+	if err != nil {
+		t.Fatalf("Reading options failed: %s", err)
+	}
+
+	if no.Template != o.Template {
+		t.Errorf("Expected template option %s, got %s", o.Template, no.Template)
+	}
+}

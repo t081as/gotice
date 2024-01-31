7d8abd3b33307d47e4066eadfa1eba1bf6de28d7
Tobias Koch <t.koch@tk-software.de>
2024-01-31 22:56:04 +0100

    Adding html rendering (#5)

--- notice/writer.go
+++ notice/writer.go
@@ -6,8 +6,9 @@ package notice
 
 import (
 	_ "embed"
+	htmltmpl "html/template"
 	"io"
-	"text/template"
+	texttmpl "text/template"
 )
 
 //go:embed txt.tmpl
@@ -19,9 +20,23 @@ var MarkdownTemplate string // the built-in markdown template
 //go:embed html.tmpl
 var HtmlTemplate string // the built-in html template
 
-// Write generates the notice file and writes it to w using the template tmpl.
-func Write(w io.Writer, tmpl string, n []Notice) error {
-	template, err := template.New("notice").Parse(tmpl)
+// WriteText generates the notice file and writes it to w using the text template tmpl.
+func WriteText(w io.Writer, tmpl string, n []Notice) error {
+	template, err := texttmpl.New("notice").Parse(tmpl)
+	if err != nil {
+		return err
+	}
+
+	if err := template.Execute(w, n); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// WriteText generates the notice file and writes it to w using the html template tmpl.
+func WriteHtml(w io.Writer, tmpl string, n []Notice) error {
+	template, err := htmltmpl.New("notice").Parse(tmpl)
 	if err != nil {
 		return err
 	}

d32d1cc42ce566f952ed62ada414244c6c311836
Tobias Koch <t.koch@tk-software.de>
2024-01-31 22:45:18 +0100

    Adding custom json marshal (#5)

--- notice/rendering.go
+++ notice/rendering.go
@@ -4,6 +4,12 @@
 
 package notice
 
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
 const (
 	Text Rendering = iota + 1 // Rendering via text/template
 	Html                      // Rendering via html/template
@@ -27,4 +33,37 @@ var (
 // String returns the string-representation.
 func (r Rendering) String() string {
 	return RenderingName[uint(r)]
+}
+
+// ParseRendering returns a ParseRendering based on the string s.
+func ParseRendering(s string) (Rendering, error) {
+	s = strings.TrimSpace(strings.ToLower(s))
+	value, ok := RenderingValue[s]
+
+	if !ok {
+		return Rendering(0), fmt.Errorf("invalid rendering option %s", s)
+	}
+
+	return Rendering(value), nil
+}
+
+// MarshalJSON returns r as the JSON encoding of r.
+func (r Rendering) MarshalJSON() ([]byte, error) {
+	return json.Marshal(r.String())
+}
+
+// UnmarshalJSON sets *r to a copy of data.
+func (r *Rendering) UnmarshalJSON(data []byte) error {
+	var rendering string
+	var err error
+
+	if err = json.Unmarshal(data, &rendering); err != nil {
+		return err
+	}
+
+	if *r, err = ParseRendering(rendering); err != nil {
+		return err
+	}
+
+	return nil
 }

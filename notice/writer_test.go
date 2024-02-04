3e632b6cb34c6687f7dc20034abb526542832bc4
Tobias Koch <t.koch@tk-software.de>
2024-02-04 10:20:45 +0100

    Adding test for WriteHtml (#5)

--- notice/writer_test.go
+++ notice/writer_test.go
@@ -55,3 +55,49 @@ func TestWriteText(t *testing.T) {
 		t.Errorf("Expected string %s not found in generated notice file", exp)
 	}
 }
+
+func TestWriteHtml(t *testing.T) {
+	var b bytes.Buffer
+	n := []Notice{
+		{
+			Path:        "p1/test",
+			Version:     "v1.2.3",
+			LicenseText: "This is a test license",
+		},
+		{
+			Path:        "path2/anothertest",
+			Version:     "v9.8.7",
+			LicenseText: "This\nis a\nmulti\nline\nlicense",
+		},
+	}
+
+	if err := WriteHtml(&b, TextTemplate, n); err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	noticefile := b.String()
+
+	if exp := n[0].Path; !strings.Contains(noticefile, exp) {
+		t.Errorf("Expected string %s not found in generated notice file", exp)
+	}
+
+	if exp := n[1].Path; !strings.Contains(noticefile, exp) {
+		t.Errorf("Expected string %s not found in generated notice file", exp)
+	}
+
+	if exp := n[0].Version; !strings.Contains(noticefile, exp) {
+		t.Errorf("Expected string %s not found in generated notice file", exp)
+	}
+
+	if exp := n[1].Version; !strings.Contains(noticefile, exp) {
+		t.Errorf("Expected string %s not found in generated notice file", exp)
+	}
+
+	if exp := n[0].LicenseText; !strings.Contains(noticefile, exp) {
+		t.Errorf("Expected string %s not found in generated notice file", exp)
+	}
+
+	if exp := n[1].LicenseText; !strings.Contains(noticefile, exp) {
+		t.Errorf("Expected string %s not found in generated notice file", exp)
+	}
+}
